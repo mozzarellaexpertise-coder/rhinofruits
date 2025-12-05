fba918fcbd0ff3e299b2a39a7aeede0ddd7af5dd
GAT <mozzarellaexpertise@gmail.com>
2025-12-05 07:59:53 +0630

    Initial SvelteKit + Netlify + Neon setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 export default {
   preprocess: preprocess(),
   kit: {
     adapter: adapter(),
+    // Optional: override default build dir
+    // paths: { base: '' }
   }
 };
